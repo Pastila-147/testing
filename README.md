3e014f9684339e99ce5cfe52dfb594aeb371d445
Evgeniya Emelina <evg.emelina@gmail.com>
2024-09-03 21:40:03 +0200

    Badge added

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 [Руководство по настройке Webpack](https://webpack.js.org/guides/)
 [Руководство по настройке GitHub Actions](https://docs.github.com/en/actions/quickstart)
 
-[![Build status](https://ci.appveyor.com/api/projects/status/d69txdsm4wa46a7o?svg=true)](https://ci.appveyor.com/project/Pastila-147/dom)
+
+[![Build status](https://ci.appveyor.com/api/projects/status/k7935ab15n2ac40x?svg=true)](https://ci.appveyor.com/project/Pastila-147/testing)
--- src/js/cardValidator.js
+++ src/js/cardValidator.js
@@ -1,18 +1,4 @@
 // Алгоритм Луна
-// export default function cardValidator(cardNumber) {
-//   let sum = 0;
-//   const num = String(cardNumber).replace(/\D/g, '');
-//   const parity = num.length % 2 !== 0;
-//   if (num === '') {
-//     return false;
-//   }
-//   for (let i = 0; i < num.length; i += 1) {
-//     let n = parseInt(num[i], 10);
-//     sum += (parity || 0) === (i % 2) && (n *= 2) > 9 ? (n - 9) : n;
-//   }
-//   return (sum % 10) === 0;
-// }
-
 export default function cardValidator(cardNumber) {
   if (/[^0-9-\s]+/.test(cardNumber) || cardNumber.length < 13) return false;
   let sum = 0;
